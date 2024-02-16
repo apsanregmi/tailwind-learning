5487a57816467ad5efc744a29326becf8a243404
apsanregmi <mahesh.regmi@cloudproai.com>
2024-02-16 21:00:54 +0545

    signupform updated

--- pages/api/signupform/signupform.js
+++ pages/api/signupform/signupform.js
@@ -1,38 +1,38 @@
-// pages/api/signupform.js
+// // pages/api/signupform.js
 
-import dbConnect from '../../../utils/dbConnect';
-import SignupModel from '../../../models/signup'; // Make sure to import your Mongoose model
+// import dbConnect from '../../../utils/dbConnect';
+// import SignupModel from '../../../models/signup'; // Make sure to import your Mongoose model
 
-export default async function handler(req, res) {
-  await dbConnect();
+// export default async function handler(req, res) {
+//   await dbConnect();
 
-  switch (req.method) {
-    case 'POST':
-      try {
-        const { firstName, lastName, email, companyName, jobTitle, phone, subscribeNewsletter } = req.body;
+//   switch (req.method) {
+//     case 'POST':
+//       try {
+//         const { firstName, lastName, email, companyName, jobTitle, phone, subscribeNewsletter } = req.body;
 
-        // Create a new instance of your Mongoose model
-        const newSignup = new SignupModel({
-          firstName,
-          lastName,
-          email,
-          companyName,
-          jobTitle,
-          phone,
-          subscribeNewsletter,
-        });
+//         // Create a new instance of your Mongoose model
+//         const newSignup = new SignupModel({
+//           firstName,
+//           lastName,
+//           email,
+//           companyName,
+//           jobTitle,
+//           phone,
+//           subscribeNewsletter,
+//         });
 
-        // Save the data to the database
-        await newSignup.save();
+//         // Save the data to the database
+//         await newSignup.save();
 
-        res.status(201).json({ message: 'Registration successful!' });
-      } catch (error) {
-        res.status(500).json({ message: 'Error saving data', error: error.message });
-      }
-      break;
+//         res.status(201).json({ message: 'Registration successful!' });
+//       } catch (error) {
+//         res.status(500).json({ message: 'Error saving data', error: error.message });
+//       }
+//       break;
 
-    default:
-      res.status(405).json({ message: 'Method Not Allowed' });
-      break;
-  }
-}
+//     default:
+//       res.status(405).json({ message: 'Method Not Allowed' });
+//       break;
+//   }
+// }
