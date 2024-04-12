773a82d8b8b6940f9c80c05bb3e5ddbbcc111a70
apsanregmi <mahesh.regmi@cloudproai.com>
2024-04-13 00:19:18 +0545

    pagebanner remained

--- pages/demopage.js
+++ pages/demopage.js
@@ -3,11 +3,50 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="bg-yellow-600 p-10  ">
-      <h1 className="text-4xl text-blue text-center ">Welcome to my Tailwind CSS Next.js app!</h1>
-      <p className="text-center text-red-800 mt-4">Tailwind CSS is working!</p>
+    <>
+    <div class="navbar bg-gray-800">
+  <a href="#home" class="text-white px-4 py-2">Home</a>
+  <a href="#news" class="text-white px-4 py-2">News</a>
+  <div class="dropdown">
+    <button class="dropbtn text-white px-4 py-2 bg-transparent">Dropdown
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="header bg-red-500 text-white py-4 px-6">
+        <h2>Mega Menu</h2>
+      </div>
+      <div class="row">
+        <div class="column w-1/3 p-4 bg-gray-300">
+          <h3>Category 1</h3>
+          <a href="#" class="text-black py-2 block">Link 1</a>
+          <a href="#" class="text-black py-2 block">Link 2</a>
+          <a href="#" class="text-black py-2 block">Link 3</a>
+        </div>
+        <div class="column w-1/3 p-4 bg-gray-300">
+          <h3>Category 2</h3>
+          <a href="#" class="text-black py-2 block">Link 1</a>
+          <a href="#" class="text-black py-2 block">Link 2</a>
+          <a href="#" class="text-black py-2 block">Link 3</a>
+        </div>
+        <div class="column w-1/3 p-4 bg-gray-300">
+          <h3>Category 3</h3>
+          <a href="#" class="text-black py-2 block">Link 1</a>
+          <a href="#" class="text-black py-2 block">Link 2</a>
+          <a href="#" class="text-black py-2 block">Link 3</a>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+
+    </>
   );
 };
 
 export default Home;
+
+
+
+
+
+
--- src/components/PageBanner.js
+++ src/components/PageBanner.js
@@ -3,7 +3,7 @@ import React from "react";
 const PageBanner = ({ title, subtitle }) => {
   return (
     <div className="bg-[#0C2126] flex justify-start items-center h-fit pb-16 pt-14">
-      <div className="md:container mx-28 py-24 px-6 flex w-1/2 md:pt-3/4 ">
+      <div className="md:container md:mx-28 md:py-24 px-2 md:px-6 flex md:w-1/2 w-full md:pt-3/4 ">
         <div className="max-w-2xl mx-2">
           <h1 className="text-4xl lg:text-4xl text-white font-bold mb-8">
             {title}
@@ -11,29 +11,30 @@ const PageBanner = ({ title, subtitle }) => {
           <p className="text-xl lg:text-xl text-white leading-relaxed max-w-xl space-x-3">
             {subtitle}
           </p>
-          <div className="flex">
-            <li className="pt-12 flex-col sm:block">
-              <a className="hover:text-gray-200 md:pb-6 w-fit " href="/contact">
+          <div className="grid grid-cols-1 sm:grid-cols-2 pt-10">
+            
+            <li className=" grid sm:block sm:flex-row w-auto justify-center ">
+              <a className="hover:text-gray-200  w-fit " href="/contact">
                 <button className="bg-[#EDFF6C]  hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
                   Contact Us
                 </button>
               </a>
             </li>
-            <li>
-              <p className="font-normal text-white text-xl flex-col pt-14 ml-16 cursor-pointer hover:underline">
-                
+            <li className="w-auto justify-center ">
+              <p className="font-normal text-white text-xl flex-col pt-14  cursor-pointer hover:underline">
                 Book a Free Call
                 <span className="ml-2 cursor-pointer font-extrabold"> ↗</span>
               </p>
-              <div>
-                
-              </div>
             </li>
+          
+            </div>
+          </div>
+            
+            
           </div>
         </div>
-      </div>
-    </div>
-  );
+  )
+        
 };
 
 export default PageBanner;
