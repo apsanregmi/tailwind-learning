ab1616e3bf2ec07f881a3e8d4cfc977f94dbbc87
apsanregmi <mahesh.regmi@cloudproai.com>
2024-04-13 00:59:09 +0545

    pagebannerfinished

--- src/components/PageBanner.js
+++ src/components/PageBanner.js
@@ -11,30 +11,25 @@ const PageBanner = ({ title, subtitle }) => {
           <p className="text-xl lg:text-xl text-white leading-relaxed max-w-xl space-x-3">
             {subtitle}
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 pt-10">
-            
-            <li className=" grid sm:block sm:flex-row w-auto justify-center ">
-              <a className="hover:text-gray-200  w-fit " href="/contact">
-                <button className="bg-[#EDFF6C]  hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
+          <div class="flex flex-col items-center md:items-start sm:flex-row sm:justify-left space-y-4 sm:space-y-0 sm:space-x-4 pt-10">
+            <div>
+            <button className="bg-[#EDFF6C]  hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
                   Contact Us
-                </button>
-              </a>
-            </li>
-            <li className="w-auto justify-center ">
-              <p className="font-normal text-white text-xl flex-col pt-14  cursor-pointer hover:underline">
-                Book a Free Call
-                <span className="ml-2 cursor-pointer font-extrabold"> ↗</span>
+               </button>
+            </div>
+            <div>
+            <p className="font-normal text-white text-xl flex-col pt-2  cursor-pointer hover:underline">
+                 Book a Free Call
+                 <span className="ml-2 cursor-pointer font-extrabold"> ↗</span>
               </p>
-            </li>
-          
             </div>
           </div>
-            
-            
-          </div>
         </div>
-  )
-        
+      </div>
+    </div>
+  );
 };
 
 export default PageBanner;
+
+
