eeb4518e8c340c4540e23c2f5e1e7614e2ce5959
apsanregmi <mahesh.regmi@cloudproai.com>
2024-04-21 13:59:15 +0545

    industries section

--- src/components/Industries/IndustriesImage.js
+++ src/components/Industries/IndustriesImage.js
@@ -3,61 +3,73 @@ import React from "react";
 const IndustriesImage = () => {
   return (
     <div className="bg-[#0C2126] ">
-      <div className="container pt-16">
+      <div className="container mt-16">
         <h4 className="text-[#EDFF6C] font-bold text-sm  md:p-6 p-2 pt-4 md:pt-10">
           Industries
         </h4>
         <p className="text-white text-3xl md:w-1/2 lg:w-1/3 sm:p-10 p-2">
-        The wide spectrum of industries we serve
+          The wide spectrum of industries we serve
         </p>
         <div className="images grid grid-cols-1 sm:grid-cols-3 lg:flex pb-16 pt-4 gap-4 justify-center sm:justify-start">
-        <div class="absolute w-193 h-296 top-1969 left-205 gap-0 border-l-16 border-t-16 border-blue-500 rounded-tl-16 opacity-0">
+        <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
               src="/assets/images/industries/airlinesImage.svg"
-              alt="Airlines"
-              className="block w-full h-full object-cover"
+              alt="Education"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Airlines</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-right py-2">
+              Airlines
+            </p>
           </div>
-          <div className="bg-white rounded-md cursor-pointer h-52 w-52">
+          <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
-              src="/assets/images/industries/automotiveImage.svg"
-              alt="Automotive"
-              className="block w-full h-full object-cover"
+              src="/assets/images/industries/AutomotiveImage.svg"
+              alt="Education"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Automotive</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-right py-2">
+              Automotive
+            </p>
           </div>
-          <div className="bg-white rounded-md cursor-pointer h-52 w-52">
+          <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
               src="/assets/images/industries/bankingAndFinancialImage.svg"
-              alt="Finance"
-              className="block w-full h-full object-cover"
+              alt="Education"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Finance</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-right py-2">
+              Banking and Financial
+            </p>
           </div>
-          <div className="bg-white rounded-md cursor-pointer h-52 w-52">
+          <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
               src="/assets/images/industries/constructionImage.svg"
-              alt="Construction"
-              className="block w-full h-full object-cover"
+              alt="Education"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Construction</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-right  py-2">
+              Construction
+            </p>
           </div>
-          <div className="bg-white rounded-md cursor-pointer h-52 w-52">
+          <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
               src="/assets/images/industries/educationImage.svg"
               alt="Education"
-              className="block w-full h-full object-cover"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Education</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white  text-right py-2">
+              Education
+            </p>
           </div>
-          <div className="bg-white rounded-md cursor-pointer h-52 w-52">
+          <div class="relative bg-white rounded-md cursor-pointer h-64 w-52">
             <img
               src="/assets/images/industries/retailsImage.svg"
               alt="Education"
-              className="block w-full h-full object-cover"
+              class="block w-full h-full object-cover rounded-md"
             />
-            <p className="text-black text-center font-bold">Retails</p>
+            <p class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-right py-2">
+              Retails
+            </p>
           </div>
         </div>
       </div>
